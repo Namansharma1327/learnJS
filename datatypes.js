fb6d44ce6b3e7e12ac24b553d7df720251c72c7a
Naman Sharma <159528188+Namansharma1327@users.noreply.github.com>
2024-06-13 13:30:10 +0000

    datatypes

---
+++ datatypes.js
@@ -0,0 +1,14 @@
+//7 primitive datatypes
+let a = "naman";
+let b = 22;
+let c ;
+let d = null;
+let isPresent = true;
+
+
+console.log(typeof(a));              //string
+console.log(typeof(b));              //number
+console.log(typeof(c));              // output : undefined
+console.log(typeof(d));              // output : object
+console.log(typeof(isPresent));      //boolean
+console.log(typeof(e));
