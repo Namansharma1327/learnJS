bdc54295d3ed4349ed328a10a83ee40c9eebfb0c
Naman Sharma <159528188+Namansharma1327@users.noreply.github.com>
2024-06-14 15:19:01 +0000

    half arraymethods completed

---
+++ arrays.js
@@ -0,0 +1,34 @@
+// // let arr = [1,2,4,"naman", true];
+// // console.log(arr);
+
+// let arr2 = new Array(1,99,36,57,4,5,47,95,9);
+// // console.log(arr2);
+// // console.log(`value at index 2 is ${arr2[2]}`);
+
+// //*************** array methods  ******* */
+// arr2.push(27);                     //pushes value at the end
+// // console.log(arr2);
+
+// arr2.pop();                         //removes the last element from array
+// // console.log(arr2);
+
+// arr2.unshift(8);                    // adds the element at the start
+// // console.log(arr2);
+
+// arr2.shift();                       // removes the first element
+// // console.log(arr2);
+
+// console.log(arr2.includes(99));        //false as 99 is not in array
+
+// console.log(arr2.indexOf(4));          //returns index of 4
+// console.log(arr2.indexOf(898));        //returns -1 as 898 is not in array
+
+let myarray = [1,3,5,6,8,4,2,9,7,36,41];
+console.log(myarray.slice(2,5));         //[5,6,8] doesnt include the last index
+console.log(myarray);                     //complete original array
+
+console.log(myarray.splice(2,5));        //[5,6,8,4] also includes the last index
+console.log(myarray);                    //[1,2,9,7,36,41] changed the original array after splice
+
+
+
